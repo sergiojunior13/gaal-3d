7431460ab1701c60fb5e12ca1e89f5bf96295190
Sergio Junior <sergiaraujo15@gmail.com>
2026-03-08 15:26:44 -0300

    fix(LAlgebra): now it uses the Matrix class

--- src/math/linear-algebra/linear-algebra.ts
+++ src/math/linear-algebra/linear-algebra.ts
@@ -1,6 +1,6 @@
 import type { Point2D } from "../../objects/points/point2d";
 import { Vector2D } from "../../objects/vectors/vector2d";
-import type { Matrix } from "../types/matrices";
+import { Matrix } from "../matrix";
 
 export type Coords = Vector2D | Point2D | { x: number; y: number };
 
@@ -13,12 +13,9 @@ export class LAlgebra {
     }
 
     static subtract(vec1: Coords, vec2: Coords) {
-        const vecInverse = {
-            x: -vec2.x,
-            y: -vec2.y,
-        };
+        const negative_vector_2 = this.scale(vec2, -1);
 
-        return this.sum(vec1, vecInverse);
+        return this.sum(vec1, negative_vector_2);
     }
 
     static scale(vec: Coords, scalar: number) {
@@ -33,36 +30,30 @@ export class LAlgebra {
     }
 
     static multiply(A: Matrix, B: Matrix): Matrix | null {
-        const nRows1 = A.length;
-        const nCols1 = A[0].length;
-
-        const nRows2 = B.length;
-        const nCols2 = B[0].length;
-
-        if (nRows1 === 0 || nRows2 === 0) return null;
+        if (A.n_rows === 0 || B.n_rows === 0) return null;
 
         // Verifica se pode multiplicar
-        if (nCols1 !== nRows2) {
+        if (A.n_cols !== B.n_rows) {
             console.error(`Erro: não é possível multiplicar essas matrizes.`);
             return null;
         }
 
         const C: number[][] = [];
 
-        for (let i = 0; i < nRows1; i++) {
+        for (let i = 0; i < A.n_rows; i++) {
             C[i] = [];
 
-            for (let j = 0; j < nCols2; j++) {
+            for (let j = 0; j < B.n_cols; j++) {
                 let sum = 0;
 
-                for (let k = 0; k < nCols1; k++) {
-                    sum += A[i][k] * B[k][j];
+                for (let k = 0; k < A.n_cols; k++) {
+                    sum += A.values[i][k] * B.values[k][j];
                 }
 
                 C[i][j] = sum;
             }
         }
 
-        return C;
+        return new Matrix(C);
     }
 }
