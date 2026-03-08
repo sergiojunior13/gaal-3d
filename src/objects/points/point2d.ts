f046e53397a7a8ac0a23bdd274f684a2a3bccc23
Sergio Junior <sergiaraujo15@gmail.com>
2026-03-08 15:48:34 -0300

    feat(Point2D): add custom radius option

--- src/objects/points/point2d.ts
+++ src/objects/points/point2d.ts
@@ -4,13 +4,15 @@ export class Point2D implements Object2D {
     x = 0;
     y = 0;
     color = "#f00";
+    radius = 0.07;
 
-    constructor(p: { x: number; y: number }, options?: { color?: string }) {
+    constructor(p: { x: number; y: number }, options?: { color?: string; radius?: number }) {
         this.x = p.x;
         this.y = p.y;
 
         if (options) {
             if (options.color) this.color = options.color;
+            if (options.radius) this.radius = options.radius;
         }
     }
 
@@ -20,14 +22,10 @@ export class Point2D implements Object2D {
         ctx.beginPath();
 
         ctx.fillStyle = this.color;
-        ctx.arc(
-            this.x * scale,
-            this.y * scale,
-            0.07 * zoom * unit,
-            0,
-            2 * Math.PI
-        );
+        ctx.arc(this.x * scale, this.y * scale, this.radius * zoom * unit, 0, 2 * Math.PI);
 
         ctx.fill();
+
+        ctx.closePath();
     }
 }
